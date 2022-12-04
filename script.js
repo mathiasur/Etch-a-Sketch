ca0a79c09bb976ade0cd6f1e79384892effd5016
mathiasur <mathias.urtheil@gmail.com>
2022-12-04 20:21:16 -0300

    Create 16x16 grid

---
+++ script.js
@@ -0,0 +1,15 @@
+const $container = document.querySelector('.container');
+
+for(let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.classList.add(`rw${i}`);
+    $container.appendChild(row);
+
+    for(let j = 0; j < 16; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        row.appendChild(square);
+    }
+
+}
