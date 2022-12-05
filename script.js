875704ede8889dae31481dd058843ff12e36ec1a
mathiasur <mathias.urtheil@gmail.com>
2022-12-04 22:08:52 -0300

    Add functionality

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 const $container = document.querySelector('.container');
+const $body = document.querySelector('body');
 
-for(let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.classList.add(`rw${i}`);
-    $container.appendChild(row);
+let mouseDown = 0;
 
-    for(let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
+document.addEventListener('mousedown', (e) => print(e));
+$body.addEventListener('mouseup', () => mouseDown = 0 );
+
+createGrid(32, 32);
+
+
+function createGrid(w, h) {
+
+    $container.classList.add('borders');
+
+    for (let i = 0; i < w; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.classList.add(`rw${i}`);
+        $container.appendChild(row);
+
+        for (let j = 0; j < h; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            row.appendChild(pixel);
+        }
+
     }
 
 }
+
+function print(e) {
+
+    let px = e.target;
+
+    if (px.className === 'pixel') {
+        
+        mouseDown = 1; 
+
+        console.log(px);
+
+        px.style = 'background-color: black';
+
+        const $pixel = document.querySelectorAll('.pixel');
+        
+        $pixel.forEach(pixel => {
+
+            pixel.ondragstart = () => false;
+         
+            pixel.addEventListener('mouseover', () => {
+                if (mouseDown === 1) 
+                    pixel.style = 'background-color: black';
+            });
+
+            pixel.addEventListener('mouseup', () => {
+                mouseDown = 0;
+            });
+
+        });
+    }
+}
+
